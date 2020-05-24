9d275e7e5e05ad777ed76c441721a7d544221fdc
DuncantheeDuncan <mthembup69@gmail.com>
2020-05-24 02:33:06 +0200

    herouku commit

--- src/main/java/TempRun.java
+++ src/main/java/TempRun.java
@@ -7,10 +7,21 @@ import portfolio.aboutme.works.ProjectProcessor;
 public class TempRun {
 
 
+static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+
     public static void main(String[] args) throws CheckNumberException {
 
         Mapping mapping = new Mapping();
 
+staticFiles.location("/public");
+        getHerokuAssignedPort();
 
             new ProjectProcessor().projects(
                     "first project",
