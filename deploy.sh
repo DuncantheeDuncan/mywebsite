589da32033c46579cdde3b580e27333967180e03
DuncantheeDuncan <mthembup69@gmail.com>
2020-05-24 02:21:20 +0200

    herouku commit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #  to remove the existing app uncomment below line
 # git remote rm heroku 
-git init
+# git init #  run once
 git add .
-git commit -m "herouku commit"
-heroku create
+git commit -m "herouku commit" # not needed for heruku only for git
+# heroku create # run once 
 mvn clean heroku:deploy
