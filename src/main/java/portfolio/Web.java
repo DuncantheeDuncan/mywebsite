3464195f7304ad85c4014e7d3147ca280750746a
DuncantheeDuncan <mthembup69@gmail.com>
2020-06-06 01:31:47 +0200

    herouku commit

--- src/main/java/portfolio/Web.java
+++ src/main/java/portfolio/Web.java
@@ -33,7 +33,7 @@ public class Web {
             model.put("menu", lp.menu);
             model.put("home", lp.home);
             model.put("about", lp.about);
-            model.put("services", lp.service);
+            model.put("service", lp.service);
             model.put("work", lp.work);
             model.put("contact", lp.contact);
             model.put("title",lp.title);
