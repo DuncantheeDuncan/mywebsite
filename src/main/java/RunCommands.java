bfbd8f14d5563e7739ec7608d0b28a785e107042
DuncantheeDuncan <mthembup69@gmail.com>
2020-06-05 23:49:10 +0200

    herouku commit

--- src/main/java/RunCommands.java
+++ src/main/java/RunCommands.java
@@ -1,36 +1,36 @@
-import portfolio.landingpage.LandingPage;
+// import portfolio.landingpage.LandingPage;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class RunCommands {
+// public class RunCommands {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        LandingPage lp = new LandingPage();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("give a command ...");
-        String command = scanner.nextLine();
+//         LandingPage lp = new LandingPage();
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("give a command ...");
+//         String command = scanner.nextLine();
 
-        String[] instruction = command.split(" ");
+//         String[] instruction = command.split(" ");
 
-        if (instruction[0].equals("add") && instruction[1].equals("skills")){
+//         if (instruction[0].equals("add") && instruction[1].equals("skills")){
 
-            if (instruction.length > 3){
+//             if (instruction.length > 3){
 
-                for (String S: instruction){
-                    //FIXME need to make commands more dynamic
-                }
-                lp.addSkills("d");
-            }
-        }
+//                 for (String S: instruction){
+//                     //FIXME need to make commands more dynamic
+//                 }
+//                 lp.addSkills("d");
+//             }
+//         }
 
 
-//        if ("add skill".equals(command)){
-//            LandingPage lp = new LandingPage();
-//
-////            lp.addSkills()
-//
-//
-//        }
-    }
-}
+// //        if ("add skill".equals(command)){
+// //            LandingPage lp = new LandingPage();
+// //
+// ////            lp.addSkills()
+// //
+// //
+// //        }
+//     }
+// }
